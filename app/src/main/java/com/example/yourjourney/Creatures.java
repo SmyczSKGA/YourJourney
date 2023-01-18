3558d36333cfee9640b1e37a6f3fbe332f365bea
Kasandra <kamilmelcer3@gmail.com>
2023-01-18 19:54:21 +0100

    New creatures

--- app/src/main/java/com/example/yourjourney/Creatures.java
+++ app/src/main/java/com/example/yourjourney/Creatures.java
@@ -3,24 +3,31 @@ package com.example.yourjourney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.*;
 
 
 public class Creatures extends AppCompatActivity {
 
-
+    ArrayList<String> creatureList = new ArrayList<String>();
 
     public class Creature {
-        public final String name, String attack, String defence, String damage, String armor, String img;
+        public final String name, attack, defence, gold, img;
 
-        public Creature(String name, String attack, String defence, String damage, String armor, String img) {
+        public Creature(String name, String attack, String defence, String gold, String img) {
             this.name = name;
             this.attack = attack;
             this.defence = defence;
-            this.damage = damage;
-            this.armor = armor;
+            this.gold = gold;
             this.img = img;
         }
     }
 
-    public static ArrayList<Creature> studenci=new ArrayList<Creature>();
+    Creature creature0 = new Creature("Wojownik", "3", "3", "0", "");
+    Creature creature1 = new Creature("Berserker", "5", "1", "0", "");
+    Creature creature2 = new Creature("Łotrzyk", "1", "5", "0", "");
+    Creature creature3 = new Creature("Dzik", "2", "3", "1", "");
+    Creature creature4 = new Creature("Goblin", "4", "3", "2", "");
+    Creature creature5 = new Creature("Ork", "5", "5", "4", "");
+
+    ArrayList<Creature> arrayCreatures = new ArrayList<Creature>();
 }
