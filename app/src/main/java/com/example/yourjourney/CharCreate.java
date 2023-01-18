4d811bd318cfc85feb5897c0e3070ecbd24d5c45
Kasandra <kamilmelcer3@gmail.com>
2023-01-18 16:46:02 +0100

    base

---
+++ app/src/main/java/com/example/yourjourney/CharCreate.java
@@ -0,0 +1,38 @@
+package com.example.yourjourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CharCreate extends AppCompatActivity {
+
+    public TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        login = findViewById(R.id.tv_login);
+    }
+
+
+    public void back(View v) {
+        Intent menuSwitch = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(menuSwitch);
+    }
+
+    public void reset(View v) {
+        login.setText("");
+        Toast.makeText(get)
+    }
+
+    public void confirm(View v) {
+        Intent loadSwitch = new Intent(getApplicationContext(), CharCreate.class);
+        startActivity(loadSwitch);
+    }
+}
---
+++ app/src/main/java/com/example/yourjourney/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.yourjourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView title;
+    public Button start_btn;
+    public Button load_btn;
+    public Button exit_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void start(View v) {
+        Intent startSwitch = new Intent(getApplicationContext(), CharCreate.class);
+        startActivity(startSwitch);
+    }
+
+    public void load(View v) {
+        Intent loadSwitch = new Intent(getApplicationContext(), CharCreate.class);
+        startActivity(loadSwitch);
+    }
+
+    public void exit(View v) {
+        System.exit(0);
+    }
+}
