a669571d743ce93ecf4b3745b71c1ae21b20c3d3
Kasandra <kamilmelcer3@gmail.com>
2023-01-20 18:37:50 +0100

    Saving added

--- app/src/main/java/com/example/yourjourney/CharCreate.java
+++ app/src/main/java/com/example/yourjourney/CharCreate.java
@@ -4,13 +4,16 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import static com.example.yourjourney.Creatures.Creature.CreatureList;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,6 +21,12 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class CharCreate extends AppCompatActivity {
@@ -28,6 +37,9 @@ public class CharCreate extends AppCompatActivity {
     TextView HP;
     TextView ATK;
     TextView DEF;
+    Toast NoUser;
+    Button b_zatwierdz;
+    int Gold;
 
 
     @Override
@@ -36,10 +48,12 @@ public class CharCreate extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         lv_postaci = findViewById(R.id.lv_postaci);
+        b_zatwierdz = findViewById(R.id.b_zatwierdz);
 
         HP = findViewById(R.id.tv_hp);
         ATK = findViewById(R.id.tv_atak);
         DEF = findViewById(R.id.tv_obrona);
+        Gold = 0;
 
         Heroes = getResources().getStringArray(R.array.characters);
 
@@ -53,32 +67,76 @@ public class CharCreate extends AppCompatActivity {
         lv_postaci.setOnItemClickListener(CharPreviewDesc);
 
         login = findViewById(R.id.tv_login);
+        NoUser = Toast.makeText(this, "",Toast.LENGTH_LONG);
+        NoUser.setText("Wpisz nazwę postaci");
 
+        b_zatwierdz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (login.getText().toString().equals("")) {
+                    NoUser.show();
+                }
+                else {
+                    String filename = login.getText().toString();
+                    String content = HP.getText().toString() +", "+ATK.getText().toString() +", "+DEF.getText().toString();
+
+                    try {
+                        saveTextAsFile(filename, content);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Intent loadSwitch = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(loadSwitch);
+                }
+            }
+        });
+
+    }
+
+    public void saveTextAsFile(String filename, String content) throws IOException {
+        String fileName = filename + ".txt";
+
+        //create
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
+
+        //write to
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(content.getBytes());
+            fos.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            Toast.makeText(this, "File not found!", Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error occured!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     AdapterView.OnItemClickListener CharPreviewDesc = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            for (int i = 0; i < lv_postaci.getCount(); i++) {
                 Log.i("Data", "dupa");
-                if (lv_postaci.isItemChecked(i) == lv_postaci.isItemChecked(0)) {
+                if (lv_postaci.isItemChecked(0) == true) {
                     HP.setText("7");
                     ATK.setText("3");
                     DEF.setText("3");
                 }
-                if (lv_postaci.isItemChecked(i) == lv_postaci.isItemChecked(1)) {
+                if (lv_postaci.isItemChecked(1) == true) {
                     HP.setText("4");
                     ATK.setText("7");
                     DEF.setText("1");
                 }
-                if (lv_postaci.isItemChecked(i) == lv_postaci.isItemChecked(2)) {
+                if (lv_postaci.isItemChecked(2) == true) {
                     HP.setText("5");
                     ATK.setText("2");
                     DEF.setText("5");
                 }
             }
-        }
-    };
+        };
 
     public void back(View v) {
         Intent menuSwitch = new Intent(getApplicationContext(), MainActivity.class);
@@ -88,10 +146,4 @@ public class CharCreate extends AppCompatActivity {
     public void reset(View v) {
         login.setText("");
     }
-
-    public void confirm(View v) {
-        Intent loadSwitch = new Intent(getApplicationContext(), CharCreate.class);
-        startActivity(loadSwitch);
-    }
-
 }
--- app/src/main/java/com/example/yourjourney/MainActivity.java
+++ app/src/main/java/com/example/yourjourney/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.yourjourney;
 
 import static com.example.yourjourney.Creatures.Creature.CreatureList;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +16,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        }
+
         if (Creatures.Creature.CreatureList.isEmpty()) {
             new Creatures.Creature("Wojownik", 7,3, 3, 0, "");
             new Creatures.Creature("Mag", 4, 7, 1, 0, "");
@@ -42,6 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("Data",CreatureList.get(index).toString());
 
+    }
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1000:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Możesz teraz zapisywać!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(this, "UWAGA! Nie możesz zapisywać!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
     }
 
     public void start(View v) {
