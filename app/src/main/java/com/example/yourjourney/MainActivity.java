d34d333aa1be9f8cf26bbfcf7e3cccf262b3ab54
47819 <47819@student.umg.edu.pl>
2023-01-20 17:05:57 +0100

    new_activity4,5

--- app/src/main/java/com/example/yourjourney/MainActivity.java
+++ app/src/main/java/com/example/yourjourney/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.yourjourney;
 
 import static com.example.yourjourney.Creatures.Creature.CreatureList;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
     public Button exit_btn;
     int index = 0;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         if (Creatures.Creature.CreatureList.isEmpty()) {
             new Creatures.Creature("Wojownik", 7,3, 3, 0, "");
