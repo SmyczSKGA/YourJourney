5d008e0fb4d6ef51525d0bc5b63cbeba972fe028
Kasandra <kamilmelcer3@gmail.com>
2023-01-23 20:06:21 +0100

    Shops functional

---
+++ app/src/main/java/com/example/yourjourney/Forest.java
@@ -0,0 +1,34 @@
+package com.example.yourjourney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Forest extends AppCompatActivity {
+
+    ImageView event;
+    TextView title;
+    TextView brief;
+    TextView tv_decision1;
+    TextView tv_decision2;
+    TextView tv_decision3;
+    TextView tv_hp;
+    TextView tv_gold;
+    Button b_decision1;
+    Button b_decision2;
+    Button b_decision3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+
+    }
+
+    public void  event1() {
+
+    }
+}
